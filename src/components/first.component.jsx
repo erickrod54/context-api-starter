2c3829a374e32864b1fdbf488ab26069cde1e4f6
Erick Rodriguez <erickrod54@gmail.com>
2022-06-26 03:54:16 -0400

    Context-api-app version 2 - FirstComponent - Features: Getting props by destructure them throught the provider and render them

--- src/components/first.component.jsx
+++ src/components/first.component.jsx
@@ -1,28 +1,47 @@
 import React from "react";
 import { AppContext } from "../context";
-/** Context-api-app version 1 - 'FirstComponent' Component -
+
+/** Context-api-app version 2 - 'FirstComponent' Component -
  *  Features: 
  *  
- *          ---> Importing and Placing 'AppContext'
- *              -to consume data from it- 
+ *          ---> Getting props by destructure them
+ *               throught the provider and render them.
  * 
- *          ---> Testing data Provided by 'AppProvider'.
+ * Notes: a single piece of data can be render just place in
+ * it.
  * 
- * Notes:  Here i bring the value 'hello' previously passed
- * throught the provider.
+ * A data set instead have to be mapped because is and array
+ * of data, elements, objects, etc.
  * 
  * */
 
 const FirstComponent = () => {
 
-    const data = React.useContext(AppContext)
+    /**here i destructure the data */
+   const { dataSet, singleData } = React.useContext(AppContext)
 
-    console.log('this is provided on FirstComponent ==> ', data)
+    console.log('this is provided on FirstComponent ==> ', dataSet )
+    
     return(
         <>
-        <h2>First Component</h2>
-        <h4>this value is provided to use on 
-                the first component: { data }</h4>
+            <h2 className="title-span">First Component</h2>
+            <h3> I provided a data using context:</h3>
+            {/**here i place it to render */}
+            <h4><span className="single-data-span">A single data: </span>{singleData}</h4>
+
+            <h4>A 'dataSet' data: </h4>
+            {/**here i mapped to render the data set */}
+            {dataSet.map((item) => {
+                const { id, text, comment } = item;
+                return(
+                    <div key={id}>
+                        <h5> Item # {id}</h5>
+                        <h5> <span className="description-span">the content is:</span></h5>
+                        <h6>{text}</h6>
+                        <h5> A comment for the item: {comment}</h5>
+                    </div>
+                )
+            })}
         </>
     )
 }
