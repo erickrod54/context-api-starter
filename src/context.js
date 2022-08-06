7f2ba26e36b93a345679eb1be6cfec168582634c
Erick Rodriguez <erickrod54@gmail.com>
2022-08-06 00:07:47 -0400

    Context-api-app version 3 - context js file - Features: Building an initialState, Implementing a TOGGLE_ACTION, Dispatching TOGGLE_ACTION

--- src/context.js
+++ src/context.js
@@ -1,42 +1,43 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+import { TOGGLE_ACTION } from './actions'
+import reducer from "./reducers";
 
-/** Context-api-app version 2 - context js file - Features: 
+/** Context-api-app version 3 - context js file - Features: 
  * 
- *          ---> In this version i build and pass 
- *               througt the provider features, data
- *               on as an array and as a single data.
+ *          ---> Building an 'intialState'.
  * 
- * Notes: this version tests in a simple way how to
- * pass functionalities, and data in diferent ways and
- * how to tested and render properly in the components
- * that they are going to
+ *          ---> Implementing a 'TOGGLE_ACTION' action.
+ * 
+ *          ---> Dispatching 'TOGGLE_ACTION'.
+ * 
+ * Notes: The pattern design to build a react app
+ * by the scale, is as follows:
+ * 
+ *    prop-drilling----> small applications (1 level deep)  
+ * 
+ *    context ---------> medium applications (2 level deep)
+ * 
+ *    context + actions 
+ *         + reducer---> large applications (3 or more level deep)
+ * 
+ * the levels deep depends on how much the 'props' have to
+ * drill to get to the Component that need it. also an intial
+ * state that will keep initial values for state props.
  * */
 
-//Doing this i have access to 'Provider' and 'Consumer'
+/**this is the initial state */
+const intialState = {
+    show: false,
+}
 
-/**The Consumer can be use sing 'useContext' hook*/
-
-/**the Provider i use the alias that i set
- * -this case 'AppContext'-*/
 const AppContext = React.createContext()
 
-
-/**The provider is a Component that will distribute
- * whatever i need to provide - features, states, data -*/
-
-/**i pass children to make the whole app the 'chilren'
- * of this provider, so i'll be able to provide*/
 const AppProvider = ({ children }) => {
     
-    /**here i build this state to get it on a destination
-     * component*/
-    const [ show, setShow ] = useState(false)
+    const [ state, dispatch ] = useReducer( reducer, intialState );
     
-    /**here  build a simple functionality and i'll
-     * get it using the provider on a destination 
-     * component*/
     const handleShow = () => {
-        setShow(!show)
+        dispatch({ type: TOGGLE_ACTION })
     }
 
     /**here is a single piece of data and i'll  
@@ -69,9 +70,11 @@ const AppProvider = ({ children }) => {
     
 
 return(
-    /**here i pass everything throught the provider*/
+    /**here i pass everything throught the provider,
+     * i spread/copy the state - initalState - and
+     * 'handleShow' will keep the action dispatch */
     <AppContext.Provider value={{
-        show,
+        ...state,
         handleShow,
         dataSet,
         singleData
